617c960f6c1fed54ebfb99d1e357e35f4055c4d5
K Jeevan Kumar <kjeevan5125@gmail.com>
2026-08-15 18:25:48 +0530

    decreased the value of the title

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = ({
         <section className="bg-indigo-700 py-20 mb-4">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
             <div className="text-center">
-                <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+                <h1 className="text-3xl font-extrabold text-white sm:text-5xl md:text-6xl">
                 {title}
                 </h1>
 
